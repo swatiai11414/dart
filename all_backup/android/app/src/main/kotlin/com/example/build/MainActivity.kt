0550dcc74c449ab61495034f50038fe4675084f5
swatiai11414 <168453519+swatiai11414@users.noreply.github.com>
2025-08-05 13:13:23 +0530

    Update MainActivity.kt

--- all_backup/android/app/src/main/kotlin/com/example/build/MainActivity.kt
+++ all_backup/android/app/src/main/kotlin/com/example/build/MainActivity.kt
@@ -1,21 +1,6 @@
 package com.example.file_transffer
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import android.os.Build
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "com.example/permissions"
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "getAndroidSdkInt") {
-                result.success(Build.VERSION.SDK_INT)
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
 }
